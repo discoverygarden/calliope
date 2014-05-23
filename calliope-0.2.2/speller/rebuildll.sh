9d2f7a305d09261aa2c10e70e6d3a534d85c8fc2
lutaylor <luke@discoverygarden.ca>
2014-05-23 10:44:57 -0300

    fix so that it runs with puppet

--- calliope-0.2.2/speller/rebuildll.sh
+++ calliope-0.2.2/speller/rebuildll.sh
@@ -1,32 +1,29 @@
+HASREADLINK=`which readlink`
+JAVAC=`which javac`
+JDKINCLUDEDIRNAME="include"
+getjdkinclude()
+{
+  if [ -n "$HASREADLINK" ]; then 
+    while [ -h $JAVAC ]
+    do
+      JAVAC=`readlink $JAVAC`
+    done
+    echo `dirname $(dirname $JAVAC)`/$JDKINCLUDEDIRNAME
+  else
+    echo "need readlink. please install."
+  fi
+  return 
+}
 if [ $USER = "root" ]; then
-  HASREADLINK=`which readlink`
-  JAVAC=`which javac`
-  getjdkinclude()
-  {
-    if [ -n "$HASREADLINK" ]; then 
-      while [ -h $JAVAC ]
-      do
-        JAVAC=`readlink $JAVAC`
-      done
-      echo `dirname $(dirname $JAVAC)`/$JDKINCLUDEDIRNAME
-    else
-      echo "need readlink. please install."
-      exit    
-    fi
-    return 
-  }
   if [ `uname` = "Darwin" ]; then
     LIBSUFFIX="dylib"
-    JDKINCLUDEDIRNAME="Headers"
-  else
     LIBSUFFIX="so"
-    JDKINCLUDEDIRNAME="include"
+   
   fi
-  JDKINC=`getjdkinclude`
-  gcc -c -DHAVE_MEMMOVE -DJNI -I$JDKINC -Iinclude -O0 -Wall -g3 -fPIC src/aesespeller.c
-  gcc *.o -shared -laspell -o libAeseSpeller.$LIBSUFFIX
-  cp libAeseSpeller.$LIBSUFFIX /usr/local/lib
+  gcc -c -DJNI -Iinclude -I$JDKINC -O0 -Wall -g3 -fPIC src/*.c 
+  gcc *.o -shared -L/usr/local/lib -laspell -o libAeseSpeller.$LIBSUFFIX
+  mv libAeseSpeller.$LIBSUFFIX /usr/local/lib/
+  rm *.o
 else
-	echo "Need to be root. Did you use sudo?"
+  echo "Need to be root. Did you use sudo?"
 fi
-
--- calliope-0.2.2/stripper/rebuildll.sh
+++ calliope-0.2.2/stripper/rebuildll.sh
@@ -1,32 +1,27 @@
-if [ $USER = "root" ]; then
-  HASREADLINK=`which readlink`
-  JAVAC=`which javac`
-  getjdkinclude()
-  {
-    if [ -n "$HASREADLINK" ]; then 
-      while [ -h $JAVAC ]
-      do
-        JAVAC=`readlink $JAVAC`
-      done
-      echo `dirname $(dirname $JAVAC)`/$JDKINCLUDEDIRNAME
-    else
-      echo "need readlink. please install."
-      exit    
-    fi
-    return 
-  }
-  if [ `uname` = "Darwin" ]; then
-    LIBSUFFIX="dylib"
-    JDKINCLUDEDIRNAME="Headers"
+HASREADLINK=`which readlink`
+JAVAC=`which javac`
+getjdkinclude()
+{
+  if [ -n "$HASREADLINK" ]; then 
+    while [ -h $JAVAC ]
+    do
+      JAVAC=`readlink $JAVAC`
+    done
+    echo `dirname $(dirname $JAVAC)`/$JDKINCLUDEDIRNAME
   else
-    LIBSUFFIX="so"
-    JDKINCLUDEDIRNAME="include"
+    echo "need readlink. please install."
+    exit    
   fi
-  JDKINC=`getjdkinclude`
-  gcc -c -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -DJNI -I$JDKINC -Iinclude -I../formatter/include -I../formatter/include/STIL -O0 -Wall -g3 -fPIC ../formatter/src/STIL/cJSON.c src/*.c  
-  gcc *.o -shared -lexpat -laspell -o libAeseStripper.$LIBSUFFIX
-  cp libAeseStripper.$LIBSUFFIX /usr/local/lib
+  return 
+}
+if [ `uname` = "Darwin" ]; then
+  LIBSUFFIX="dylib"
+  JDKINCLUDEDIRNAME="Headers"
 else
-	echo "Need to be root. Did you use sudo?"
+  LIBSUFFIX="so"
+  JDKINCLUDEDIRNAME="include"
 fi
-
+JDKINC=`getjdkinclude`
+gcc -c -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -DJNI -I$JDKINC -Iinclude -I../formatter/include -I../formatter/include/STIL -O0 -Wall -g3 -fPIC ../formatter/src/STIL/cJSON.c src/*.c  
+gcc *.o -shared -lexpat -laspell -o libAeseStripper.$LIBSUFFIX
+cp libAeseStripper.$LIBSUFFIX /usr/local/lib
