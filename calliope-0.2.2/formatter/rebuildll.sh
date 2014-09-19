21439085823158c80749f7197713597a7b5b8898
lutaylor <luke@discoverygarden.ca>
2014-09-19 11:47:07 -0300

    update new stuff

--- calliope-0.2.2/formatter/rebuildll.sh
+++ calliope-0.2.2/formatter/rebuildll.sh
@@ -1,28 +1,4 @@
-HASREADLINK=`which readlink`
-JAVAC=`which javac`
-getjdkinclude()
-{
-  if [ -n "$HASREADLINK" ]; then 
-    while [ -h $JAVAC ]
-    do
-      JAVAC=`readlink $JAVAC`
-    done
-    echo `dirname $(dirname $JAVAC)`/$JDKINCLUDEDIRNAME
-  else
-    echo "need readlink. please install."
-  fi
-  return 
-}
-if [ `uname` = "Darwin" ]; then
-  LIBSUFFIX="dylib"
-  JDKINCLUDEDIRNAME="Headers"
-else
-  LIBSUFFIX="so"
-  JDKINCLUDEDIRNAME="include"
-fi
-JDKINC=`getjdkinclude`
-gcc -c -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -DJNI -I$JDKINC -Iinclude -Iinclude/STIL -Iinclude/AESE -O0 -Wall -g3 -fPIC src/*.c src/AESE/*.c src/STIL/*.c 
-gcc *.o -shared -o libAeseFormatter.$LIBSUFFIX
-mv libAeseFormatter.$LIBSUFFIX /usr/local/lib/
+gcc -c -DHAVE_EXPAT_CONFIG_H -DHAVE_MEMMOVE -DJNI -I/usr/lib/jvm/java-6-oracle/include -Iinclude -Iinclude/STIL -Iinclude/AESE -O0 -Wall -g3 -fPIC src/*.c src/AESE/*.c src/STIL/*.c
+gcc *.o -shared -o libAeseFormatter.so
+mv libAeseFormatter.so /usr/local/lib/
 rm *.o
-
